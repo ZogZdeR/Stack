a3b2b8f213eaf90e0d013779814b56c6770f6a8a
ZogZdeR <astafiev.mic@gmail.com>
2025-10-05 22:56:23 +0300

    Финал додеп

--- main.cpp
+++ main.cpp
@@ -6,11 +6,18 @@
 
 int main()
 {
+    size_t a = 0;
     Stack_stucture my_stack;
     stack_initialiser (15, &my_stack);
     Stack_print(&my_stack);
     push (&my_stack, 10);
+    a = pop(&my_stack);
+    push (&my_stack, 13);
+    push (&my_stack, 12);
+    push (&my_stack, 11);
     Stack_print(&my_stack);
+    printf ("%lu", a);
+    destructor (&my_stack);
     return 0;
 }
 
